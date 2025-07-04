b147de68af9d8eaa5d1ccb32f97d26fbbaeaaf89
minhnguyent546 <minhnguyent546@gmail.com>
2025-07-04 10:46:23 +0700

    feat(api): serialize `created_at` for UserPublic

--- api/app/users/schemas.py
+++ api/app/users/schemas.py
@@ -1,7 +1,10 @@
 import enum
 from datetime import datetime
 
-from pydantic import BaseModel, ConfigDict, EmailStr
+from loguru import logger
+from pydantic import BaseModel, ConfigDict, EmailStr, field_serializer
+
+from app.core.config import timezone_vi
 
 
 class UserRole(str, enum.Enum):
@@ -40,3 +43,12 @@ class UserPublic(BaseModel):
     created_at: datetime
 
     model_config = ConfigDict(from_attributes=True)
+
+    @field_serializer("created_at")
+    def serialize_created_at(self, value: datetime, _info):
+        try:
+            value = value.astimezone(timezone_vi).replace(tzinfo=timezone_vi)
+        except Exception as e:
+            logger.error(f"Error converting timezone to {timezone_vi}: {e}. Leaving as is.")
+
+        return value.strftime("%Y-%m-%d - %H:%M:%S")
