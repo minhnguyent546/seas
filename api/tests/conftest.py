860cc0da26299477b58ea12753dbf01c521f3f68
minhnguyent546 <minhnguyent546@gmail.com>
2025-07-07 01:34:26 +0700

    ci: caught bug in 8k

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -6,7 +6,7 @@ from app.main import app
 from tests.utils.users import get_superuser_token_headers
 
 
-@pytest_asyncio.fixture(scope="session")
+@pytest_asyncio.fixture(scope="session", autouse=True)
 async def session():
     """Create a database session for the test session."""
     async with AsyncSessionLocal() as async_session_local:  # pyright: ignore[reportGeneralTypeIssues]
