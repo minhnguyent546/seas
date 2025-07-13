bc75992840c50ce817b1718fb4b63351b2b4a002
minhnguyent546 <minhnguyent546@gmail.com>
2025-07-13 10:50:41 +0700

    chore: make some lints

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,13 +1,15 @@
+from collections.abc import AsyncGenerator
+
 import pytest_asyncio
 from httpx import ASGITransport, AsyncClient
 
-from app.core.database import AsyncSessionLocal, init_db
+from app.core.database import AsyncSession, AsyncSessionLocal, init_db
 from app.main import app
 from tests.utils.users import get_superuser_token_headers
 
 
 @pytest_asyncio.fixture(scope="session", autouse=True)
-async def session():
+async def session() -> AsyncGenerator[AsyncSession, None]:
     """Create a database session for the test session."""
     async with AsyncSessionLocal() as async_session_local:  # pyright: ignore[reportGeneralTypeIssues]
         await init_db(session=async_session_local)
@@ -15,7 +17,7 @@ async def session():
 
 
 @pytest_asyncio.fixture(scope="session")
-async def client():
+async def client() -> AsyncGenerator[AsyncClient, None]:
     """Asynchronous test client for FastAPI."""
     async with AsyncClient(
         transport=ASGITransport(app=app), base_url="http://test"
